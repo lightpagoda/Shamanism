cccce1b678b2b87a721e7ab80ee831e18ab9a591
Lightpagoda <lightpagoda@gmail.com>
2014-09-27 13:54:20 -0700

    still working

---
+++ src/main/java/com/lightpagoda/shamanism/Shamanism.java
@@ -0,0 +1,37 @@
+package com.lightpagoda.shamanism;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Lightpagoda on 9/27/2014.
+ */
+
+@Mod(modid="Shamanism", name="Shamanism", version ="1.7.2-0.1")
+public class Shamanism
+{
+
+    @Mod.Instance("Shamanism")
+    public static Shamanism instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init (FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init (FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
